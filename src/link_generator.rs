e77b677c6bcfd324b97f3a1f0331086ef0c46c8f
Heru Handika <herubiolog@gmail.com>
2022-01-13 15:35:43 -0600

    Add browser options.

--- src/link_generator.rs
+++ src/link_generator.rs
@@ -1,4 +1,4 @@
-const LSU_PROXIE: &'static str = "http://libezp.lib.lsu.edu/login?url=";
+const LSU_PROXIE: &str = "http://libezp.lib.lsu.edu/login?url=";
 
 pub fn generate_proxy_link(url: &str) -> String {
     format!("{}{}", LSU_PROXIE, url)
--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,27 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::new("browser")
+                .long("browser")
+                .short('b')
+                .help("Browser choices.")
+                .takes_value(true)
+                .possible_values(&["default", "firefox", "chrome", "safari"])
+                .default_value("default")
+                .required(true),
+        )
         .get_matches();
 
     let input = args.value_of("input").unwrap();
+    let browser = args.value_of("browser").unwrap();
 
     let url = link_generator::generate_proxy_link(input);
-    open::that(url).expect("Failed to open the link!");
+
+    match browser {
+        "default" => open::that(url).expect("Failed to open the link!"),
+        _ => open::with(url, browser).expect("Failed to open the link!"),
+    }
 
     println!("DONE!");
 }
