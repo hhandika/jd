63fc31f968a2ea61fd30972ffe302f2d5988e5a4
hhandika <herubiolog@gmail.com>
2022-01-14 13:10:02 -0600

    Allow to add proxy file.

--- src/link_generator.rs
+++ src/link_generator.rs
@@ -1,5 +1,18 @@
+use std::path::Path;
+
 const LSU_PROXIE: &str = "http://libezp.lib.lsu.edu/login?url=";
+const COSTUM_PROXY_PATH: &str = "proxy.txt";
 
 pub fn generate_proxy_link(url: &str) -> String {
-    format!("{}{}", LSU_PROXIE, url)
+    let costume_path = Path::new(COSTUM_PROXY_PATH);
+    if costume_path.is_file() {
+        let proxy_url = std::fs::read_to_string(costume_path)
+            .expect("Failed to read proxy.txt")
+            .trim()
+            .to_string();
+        println!("Using proxy: {}", proxy_url);
+        format!("{}{}", proxy_url, url)
+    } else {
+        format!("{}{}", LSU_PROXIE, url)
+    }
 }
